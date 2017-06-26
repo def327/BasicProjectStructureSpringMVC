af9f09e2fb56b3a4aa6853578a7866ffd3b2aa51
def327 <def3273@gmail.com>
2017-06-26 23:52:16 +0300

    26 JUNE 2017 1) Add new name LoginController for UserController class 2) Fix dispatcher-servlet.xml for controllers scanning

--- src/main/java/com/tutorial/controllers/LoginController.java
+++ src/main/java/com/tutorial/controllers/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class UserController {
+public class LoginController {
 
     @RequestMapping(
             method = {RequestMethod.GET},
