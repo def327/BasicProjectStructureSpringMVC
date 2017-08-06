38bb74bd3de2b97cef5d64399f3f5336cb445329
def327 <def3273@gmail.com>
2017-08-06 18:57:53 +0300

    Fix UserController

--- src/main/java/com/tutorial/controllers/UserController.java
+++ src/main/java/com/tutorial/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.tutorial.controllers;
 
 import com.tutorial.dao.DataSource;
 import com.tutorial.entities.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,8 @@ import java.util.List;
 
 @Controller
 public class UserController {
+
+    private static final Logger LOGGER = LogManager.getLogger(UserController.class);
 
     @Inject
     private DataSource dataSource;
@@ -47,24 +51,27 @@ public class UserController {
     }
 
 
-    //REST WEB-SERVICES
+    //REST-ful WEB-SERVICES
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public User getUser(@PathVariable("id") BigInteger userId) {
+        LOGGER.debug("GET : /user/{}", userId);
         return dataSource.getUserById(userId);
     }
 
     @RequestMapping(value = "/users", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public List<User> getUsers() {
+        LOGGER.debug("GET : /users");
         return dataSource.getUsers();
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity<String> addUser(@RequestBody User user) {
+        LOGGER.debug("POST : /user");
         dataSource.addUser(user);
-        return new ResponseEntity(HttpStatus.ACCEPTED);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     public void setDataSource(DataSource dataSource) {
